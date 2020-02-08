4aa0783c5a28c36ed83a41ae1625a1cca0952d97
MinexAutomation <minex.automation@gmail.com>
2020-02-07 19:12:08 -0800

    Added IServiceCollection extensions.

--- source/R5T.Solitude.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Solitude.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Solitude.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+using R5T.Solgene;
+using R5T.Soltana.Standard;
+using R5T.Solutas.Standard;
+
+using R5T.Solitude.Soltana;
+
+
+namespace R5T.Solitude.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileOperator"/> implementation of <see cref="IVisualStudioSolutionFileOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileOperator(this IServiceCollection services,
+            ServiceAction<IVisualStudioSolutionFileGenerator> addSolutionFileGenerator)
+        {
+            services.AddSoltanaVisualStudioSolutionFileOperator(
+                services.AddInMemoryVisualStudioSolutionFileOperatorAction(addSolutionFileGenerator),
+                services.AddVisualStudioSolutionFileSerializerAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFileOperator"/> implementation of <see cref="IVisualStudioSolutionFileOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileOperator> AddVisualStudioSolutionFileOperatorAction(this IServiceCollection services,
+            ServiceAction<IVisualStudioSolutionFileGenerator> addSolutionFileGenerator)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileOperator>(() => services.AddVisualStudioSolutionFileOperator(addSolutionFileGenerator));
+            return serviceAction;
+        }
+    }
+}
