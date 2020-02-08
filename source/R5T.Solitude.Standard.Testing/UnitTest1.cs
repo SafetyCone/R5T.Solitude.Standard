46edac89108cf5a209cd066c4572297c4572df22
MinexAutomation <minex.automation@gmail.com>
2020-02-07 16:26:53 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Solitude.Standard.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Solitude.Standard.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
