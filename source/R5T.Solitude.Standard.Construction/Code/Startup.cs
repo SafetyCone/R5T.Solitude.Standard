7a2010f03f71e43b88cf14432a0d25dd4316fc77
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:31:18 -0800

    Added Startup class.

---
+++ source/R5T.Solitude.Standard.Construction/Code/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Bath.Console.Standard;
+using R5T.Chalandri.DropboxRivetTestingData;
+using R5T.Evosmos.CDriveTemp;
+using R5T.Odense.Standard;
+using R5T.Richmond;
+using R5T.Solgene.VS2017.Standard;
+
+
+namespace R5T.Solitude.Standard.Construction
+{
+    class Startup : StartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<Program>()
+                .AddConsoleHumanOutput()
+                .AddTemporaryDirectoryFilePathProvider()
+                .AddTestingDataDirectoryContentPathsProvider()
+                .AddVisualStudioSolutionFileOperator(services.AddVisualStudioSolutionFileGeneratorAction())
+                .AddVisualStudioSolutionFileProjectTypeGuidProvider()
+                ;
+        }
+    }
+}
